ef74c94f9e78c2ee695c1ca6a8a7b4565f29a8a1
adry <tazzmaniac51@gmail.com>
2018-03-21 21:16:31 +0100

    commit inicial

---
+++ ejercicio4/modal.js
@@ -0,0 +1,23 @@
+//Apertura y cierre de modal de BOTON IZQUIERDO
+function modalLeftShow(){
+    $('#modalIzquierda').modal('show');
+}
+function modalLeftClose() {
+    $('#modalIzquierda').modal('hide')
+}
+
+//Apertura y cierra de modal de BOTON CENTRAL
+function modalCenterShow(){
+    $('#modalCentro').modal('show');
+}
+function modalCenterClose() {
+    $('#modalCentro').modal('hide')
+}
+
+//Apertura y cierre de BOTON DERECHO
+function modalRightShow(){
+    $('#modalDerecha').modal('show');
+}
+function modalRightClose() {
+    $('#modalDerecha').modal('hide')
+}
---
+++ ejercicio5/pop.js
@@ -0,0 +1,17 @@
+function popNombre(){
+    $('.popover-dismiss').popover({
+        trigger: 'focus'
+    })
+}
+
+function popEmail(){
+    $('.popover-dismiss').popover({
+        trigger: 'focus'
+    })
+}
+
+function popAge() {
+    $('.popover-dismiss').popover({
+        trigger: 'focus'
+    })
+}
